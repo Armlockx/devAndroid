b59c3f12cfd940e6914b07ea2e15b8f43736d4ab
symphxnys <dammitbruna@gmail.com>
2018-09-04 21:56:59 -0300

    toaster e integracao

--- app/src/main/java/com/example/cnec/myapplication/MainActivity.java
+++ app/src/main/java/com/example/cnec/myapplication/MainActivity.java
@@ -93,6 +93,75 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+
+//
+//
+
+    public void somar(View v){
+        TextView tv = (TextView) findViewById(R.id.tv);
+        EditText et = (EditText) findViewById(R.id.numero1);
+        EditText pt = (EditText) findViewById(R.id.pt);
+        TextView rato = (TextView) findViewById(R.id.pegaRatao);
+
+        ConstraintLayout cl = (ConstraintLayout) findViewById(R.id.inicial);
+
+        Float num1 = Float.parseFloat(et.getText().toString());
+        Float num2 = Float.parseFloat(pt.getText().toString());
+
+        try{
+            if(num1 == NULL){
+                if(num2 == NULL){
+                    rato.setText(":::NULLSHIT::: ");
+                }
+                else{
+                    float resultado = num1 + num2;
+                    rato.setText(num1 + " + " + num2 + " = " + resultado);
+                }
+            }
+            else{
+                float resultado = num1 + num2;
+                rato.setText(num1 + " + " + num2 + " = " + resultado);
+            }
+
+        }catch (Exception e){
+            rato.setText(":::Exception::: ");
+
+        }
+    }
+
+    public void subtrair(View v){
+        TextView tv = (TextView) findViewById(R.id.tv);
+        EditText et = (EditText) findViewById(R.id.numero1);
+        EditText pt = (EditText) findViewById(R.id.pt);
+        TextView rato = (TextView) findViewById(R.id.pegaRatao);
+
+        ConstraintLayout cl = (ConstraintLayout) findViewById(R.id.inicial);
+
+        Float num1 = Float.parseFloat(et.getText().toString());
+        Float num2 = Float.parseFloat(pt.getText().toString());
+
+        try{
+            if(num1 == NULL){
+                if(num2 == NULL){
+                    rato.setText(":::NULLSHIT::: ");
+                }
+                else{
+                    float resultado = num1 - num2;
+                    rato.setText(num1 + " + " + num2 + " = " + resultado);
+                }
+            }
+            else{
+                float resultado = num1 - num2;
+                rato.setText(num1 + " + " + num2 + " = " + resultado);
+            }
+
+        }catch (Exception e){
+            rato.setText(":::Exception::: ");
+
+        }
+    }
+}
+
 /*///////////////////////////////////////////////////////////////////////////////////////////////////
 
     public void pressButton(View v){
@@ -285,70 +354,3 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /*////////////////////////////////////////////////////////////
-
-    public void somar(View v){
-        TextView tv = (TextView) findViewById(R.id.tv);
-        EditText et = (EditText) findViewById(R.id.numero1);
-        EditText pt = (EditText) findViewById(R.id.pt);
-        TextView rato = (TextView) findViewById(R.id.pegaRatao);
-
-        ConstraintLayout cl = (ConstraintLayout) findViewById(R.id.inicial);
-
-        Float num1 = Float.parseFloat(et.getText().toString());
-        Float num2 = Float.parseFloat(pt.getText().toString());
-
-        try{
-            if(num1 == NULL){
-                if(num2 == NULL){
-                    rato.setText(":::NULLSHIT::: ");
-                }
-                else{
-                    float resultado = num1 + num2;
-                    rato.setText(num1 + " + " + num2 + " = " + resultado);
-                }
-            }
-            else{
-                float resultado = num1 + num2;
-                rato.setText(num1 + " + " + num2 + " = " + resultado);
-            }
-
-        }catch (Exception e){
-            rato.setText(":::Exception::: ");
-
-        }
-    }
-
-    public void subtrair(View v){
-        TextView tv = (TextView) findViewById(R.id.tv);
-        EditText et = (EditText) findViewById(R.id.numero1);
-        EditText pt = (EditText) findViewById(R.id.pt);
-        TextView rato = (TextView) findViewById(R.id.pegaRatao);
-
-        ConstraintLayout cl = (ConstraintLayout) findViewById(R.id.inicial);
-
-        Float num1 = Float.parseFloat(et.getText().toString());
-        Float num2 = Float.parseFloat(pt.getText().toString());
-
-        try{
-            if(num1 == NULL){
-                if(num2 == NULL){
-                    rato.setText(":::NULLSHIT::: ");
-                }
-                else{
-                    float resultado = num1 - num2;
-                    rato.setText(num1 + " + " + num2 + " = " + resultado);
-                }
-            }
-            else{
-                float resultado = num1 - num2;
-                rato.setText(num1 + " + " + num2 + " = " + resultado);
-            }
-
-        }catch (Exception e){
-            rato.setText(":::Exception::: ");
-
-        }
-    }
-
-    /////////////////////////////////////////////////////////////
-}
